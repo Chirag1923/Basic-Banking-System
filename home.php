384ec74f480f07069d8da0a43f679e1e6e88f5f5
PARMAR CHIRAGBHAI ARVINDBHAI <66724676+Chirag1923@users.noreply.github.com>
2022-05-21 16:48:40 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,28 @@
+
+<?php
+  include 'style.php';
+?>
+
+<center><h1>Welcome to KKRC Bank</h1></center>
+
+<div class="bg">
+  <div class="center">
+    <ul>
+      <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">View Users</button></a></li>
+      <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Trasnfer Money</button></a></li>
+      <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">View Transfer History</button></a></li>
+    </ul>
+  </div>
+</div>
+
+<div id="container">
+    <div id="header">© 2022. Made by chirag parmar <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
+
+</body>
+</html>
+
+
+
+
---
+++ style.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>KKRC Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+
+</head>
+
+
+<body style="background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+	height: 100vh;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}">
+
+
+
+
+ 
